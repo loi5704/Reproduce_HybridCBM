ef6241341538b5c1adf80d50ae0280a2d3944a38
abc@gmail.com <queolewlew@gmail.com>
2026-03-07 10:50:48 +0700

    a

--- models/cbms/baseCBM.py
+++ models/cbms/baseCBM.py
@@ -108,3 +108,83 @@ class CBM(nn.Module):
         sim_score = self.scale * img_feat @ concept_features.T  # B, C
         logits = self.classifier(sim_score)
         return logits
+
+    def train_concept(self, image, label):
+        final_loss = 0
+        if self.config.lambda_discri > 0:
+            image = image / image.norm(dim=-1, keepdim=True)
+            discri_loss = self.discri_loss(image, self.conceptbank.dynamic_features, label, self.classes_embeddings)
+            final_loss += discri_loss
+            
+        if self.config.lambda_ort > 0:
+            ort_loss = self.ortho_loss(self.conceptbank.dynamic_bank.concept_features, self.conceptbank.static_bank.concept_features)
+            final_loss += ort_loss
+            
+        if (self.config.lambda_align > 0 and self.conceptbank.dynamic_features.shape[0] > 0 and self.conceptbank.static_features.shape[0] > 0):
+            align_loss = self.align_loss(self.conceptbank.dynamic_bank.concept_features, self.conceptbank.static_bank.concept_features)
+            final_loss += align_loss
+            
+        return final_loss
+    
+    def train_classifier(self, image, label):
+        if self.config.use_normalize:
+            image = image / image.norm(dim=-1, keepdim=True)
+            
+        logits = self.forward(image)
+        cls_loss = self.cls_loss(logits, label)
+        final_loss = cls_loss
+
+        if self.config.lambda_l1 > 0:
+            row_l1_norm = torch.linalg.vector_norm(self.classifier.weight, ord=1, dim=-1).mean()
+            final_loss += self.config.lambda_l1 * row_l1_norm
+
+        return final_loss
+    
+    @torch.no_grad()
+    def get_topk_concepts_for_img(self, img, y, k=10):
+        """Xuất ra top K khái niệm quan trọng nhất của một bức ảnh cụ thể (Explainability)"""
+        img = (img / img.norm(dim=-1, keepdim=True)).cpu()
+        sim = img @ self.concept_features.cpu().T  # B, C
+        attention = sim * self.classifier.weight[y].cpu()  # B, C, N
+        
+        if self.config.num_static_concept > 0:
+            value, idx = torch.topk(attention[:, :self.config.num_static_concept], k=k, dim=-1)
+            static_topk = self.conceptbank.get_static_concepts(idx=idx, dataframe=True)
+            static_topk['value'] = value.view(-1).tolist()
+            static_topk.to_csv(self.exp_root.joinpath('topk_static_img_concepts.csv'), index=False)
+
+        if self.config.num_dynamic_concept > 0:
+            value, idx = torch.topk(attention[:, self.config.num_static_concept:], k=k, dim=-1)
+            dynamic_topk = self.conceptbank.get_dynamic_concepts(idx=idx, dataframe=True)
+            dynamic_topk['value'] = value.view(-1).tolist()
+            dynamic_topk.to_csv(self.exp_root.joinpath('topk_dynamic_img_concepts.csv'), index=False)
+
+        value, idx = torch.topk(attention, k=k, dim=-1)
+        hybrid_topk = self.conceptbank.get_concepts(idx=idx, dataframe=True)
+        hybrid_topk['value'] = value.view(-1).tolist()
+        hybrid_topk.to_csv(self.exp_root.joinpath('topk_hybrid_img_concepts.csv'), index=False)
+
+    @torch.no_grad()
+    def save_topk_concepts_for_class(self):
+        """Lưu lại top K khái niệm đặc trưng nhất cho từng nhãn/lớp (Global Explanation)"""
+        if self.config.num_static_concept > 0:
+            k = min(10, self.config.num_static_concept)
+            weight = self.classifier.weight[:, :self.config.num_static_concept].cpu()
+            value, static_idx = torch.topk(weight, k=k)
+            static_topk = self.conceptbank.get_static_concepts(idx=static_idx, dataframe=True)
+            static_topk['value'] = value.view(-1).tolist()
+            static_topk.to_csv(self.exp_root.joinpath('topk_static_concepts.csv'), index=False)
+            
+        if self.config.num_dynamic_concept > 0:
+            k = min(10, self.config.num_dynamic_concept)
+            weight = self.classifier.weight[:, self.config.num_static_concept:].cpu()
+            value, dynamic_idx = torch.topk(weight, k=k)
+            dynamic_topk = self.conceptbank.get_dynamic_concepts(idx=dynamic_idx, dataframe=True)
+            dynamic_topk['value'] = value.view(-1).tolist()
+            dynamic_topk.to_csv(self.exp_root.joinpath('topk_dynamic_concepts.csv'), index=False)
+
+        weight = self.classifier.weight.cpu()
+        value, idx = torch.topk(weight, k=20)
+        hybrid_topk = self.conceptbank.get_concepts(idx=idx)
+        hybrid_topk['value'] = value.view(-1).tolist()
+        hybrid_topk.to_csv(self.exp_root.joinpath('topk_hybrid_concepts.csv'), index=False)
---
+++ train_HybridCBM.sh
@@ -0,0 +1,37 @@
+export CUDA_VISIBLE_DEVICES=0
+
+
+python train_HybridCBM.py \
+    --dataset HAM10000 \
+    --data_root /kaggle/input/datasets/loinguyen57/ham10000-merge/ham10000_images \
+    --exp_root /kaggle/working/experiments/HAM10000_run_1 \
+    --device cuda:0 \
+    --clip_model ViT-L/14 \
+    --translator_path /kaggle/input/models/loinguyen57/translators/pytorch/default/1/translators-best.pt \
+    \
+    --weight_init_method rand \
+    --train_mode joint \
+    --scale 0.1 \
+    --use_normalize False \
+    --num_class 7 \
+    --num_static_concept 50 \
+    --num_dynamic_concept 50 \
+    --concept_select_fn submodular \
+    --submodular_weights 1e7 0.1\
+    \
+    --batch_size 128 \
+    --num_workers 4 \
+    --pin_memory False \
+    --use_img_features True \
+    --force_compute False \
+    --max_epochs 50 \
+    --lr 0.001 \
+    --concept_lr 0.001 \
+    --n_shots all \
+    \
+    --lambda_l1 0.001 \
+    --lambda_discri 1.0 \
+    --lambda_discri_alpha 0.5 \
+    --lambda_discri_beta 0.5 \
+    --lambda_ort 0.1 \
+    --lambda_align 0.01
